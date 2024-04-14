7ba02892c8035623598b74f2b14a221e9684414d
karinabertolazzo <mota.de.santana@hotmail.com>
2024-04-14 05:27:10 -0300

    costs

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,10 @@
+function NewProject () {
+    return(
+        <div>
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para depois adicionar os serviços</p>
+        </div>
+)
+}
+
+export default NewProject
